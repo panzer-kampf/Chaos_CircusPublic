79925366e82019b3851f2c0df444e5779de42236
panzer-kampf <80621093+panzer-kampf@users.noreply.github.com>
2023-04-23 01:42:55 -0500

    pretty cool project  im working on
    This project has a boatload of errors and bugs. If you want you can help with some of the bugs!

--- world.sh
+++ world.sh
@@ -4,14 +4,17 @@ dpkg -s htop && echo "check" || sudo apt install htop
 dpkg -s bpytop && echo "check" || sudo apt install bpytop
 dpkg -s whiptail && echo "check" || sudo apt install whiptail
 sleep 3
-PASS="42297482"
+PASS="123456"
+#CMNT1: Problem with char being pre imput, find way to fix (Note by Panzer)
 Bpytop="Bpytop"
 Htop="Htop"
 mopres=
+#RefToCMNT1: I think it has something to do with this if statement but im not too sure (Note by Panzer)
 if (whiptail --passwordbox "Enter password"  20 40 == PASS);then
     sleep 2
     $(mopres)="whiptail --menu "choose system to run" 20 40 3 \ Bpytop "must be in fullscreen" \ Htop \ exit"
 else
+#CMNT2: Find a way to restart the program in place of this. Maybe put it in a function to be called on? (Note by Panzer)
     echo "please restart the program to try again"
     sleep 3
     exit
